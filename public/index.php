205f685e67e26c031d054459a7590e7867879c17
Elionai Pereira <pereira.elionai92@gmail.com>
2020-08-18 06:41:15 -0300

    Compartilhando miniframework com a comunidade web

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+
+	require_once "../vendor/autoload.php";
+
+	$route = new \App\Route;
+	//echo "Isso está funcionando";
+	//echo "<hr />";
+	//print_r($route->getUrl());
+	//echo "<hr />";
+	//echo "<pre>";
+	//print_r($route->getRoutes());
+	//echo "</pre>";
+?>
